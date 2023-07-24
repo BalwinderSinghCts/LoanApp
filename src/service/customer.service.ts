dc038c669d6891f18a2880df7b2164f24d592236
Balwinder.singhNTG <balwinder.singh@ntgfreight.com>
2023-07-24 16:23:42 +0530

    commit

--- src/service/customer.service.ts
+++ src/service/customer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable, catchError, throwError } from "rxjs";
 import { HttpRequestHandlerService } from "./http.service";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable()
 export class CustomerService {
@@ -9,10 +10,27 @@ export class CustomerService {
     /**
      *
      */
-    constructor(private http: HttpRequestHandlerService) {
+    constructor(private jwtServiceHelper: JwtHelperService, private http: HttpRequestHandlerService) {
 
     }
     public getCustomerDetails(id: string): Observable<any> {
         return this.http.get(this.apiURL + id)
+    }
+    tokenisvalidorno(): boolean {
+        if (this.jwtServiceHelper.isTokenExpired()) {
+            return true;
+        }
+        else
+            return false;
+    }
+    tokenDetails(): any {
+        console.log(this.jwtServiceHelper.decodeToken(JSON.parse(localStorage.getItem('userData') || '{}')['access_token'] ))
+        return this.jwtServiceHelper.decodeToken(JSON.parse(localStorage.getItem('userData') || '{}')['access_token'] );
+        
+    }
+    tokenExp(): any {
+        console.log(this.jwtServiceHelper.getTokenExpirationDate())
+        return this.jwtServiceHelper.getTokenExpirationDate();
+        
     }
 }
